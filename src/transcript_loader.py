22d3645c44d68354cf6a7cc5e16feadbc94b1c25
bhoopender <bhoopenderbaghel888@gmail.com>
2026-03-13 17:39:07 +0530

    add multiple language support

--- src/transcript_loader.py
+++ src/transcript_loader.py
@@ -1,9 +1,17 @@
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 
 def get_transcript(video_id):
+
     try:
-        transcript_list = YouTubeTranscriptApi().fetch(video_id, languages=['en'])
+        
+        languages = ['en', 'hi', 'es', 'fr']
+
+        transcript_list = YouTubeTranscriptApi().fetch(video_id, languages=languages)
+
         transcript = " ".join(chunk.text for chunk in transcript_list)
+
         return transcript
+
     except TranscriptsDisabled:
+        print("Transcripts are disabled for this video.")
         return None
